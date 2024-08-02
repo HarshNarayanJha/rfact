f94999be058c62fffccd54bc77a8dfb2dbd8ea9e
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-08-02 17:25:22 +0530

    minimum working code!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,41 @@
-/// GOAL: Fetch random fact from https://uselessfacts.jsph.pl/ and display it to the user _nicely_formatted_
+use clap::Parser;
+use reqwest;
+use serde::{Deserialize, Serialize};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Show today's useless fact instead of a random one
+    #[arg(short, long)]
+    today: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Fact {
+    id: String,
+    text: String,
+    source: String,
+    source_url: String,
+    language: String,
+    permalink: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if args.today {
+        let resp: Fact =
+            match reqwest::blocking::get("https://uselessfacts.jsph.pl/api/v2/facts/today") {
+                Ok(resp) => resp.json().unwrap(),
+                Err(err) => panic!("Error: {}", err),
+            };
+        println!("Today's Random Fact\n{:#?}", resp);
+    } else {
+        let resp: Fact =
+            match reqwest::blocking::get("https://uselessfacts.jsph.pl/api/v2/facts/random") {
+                Ok(resp) => resp.json().unwrap(),
+                Err(err) => panic!("Error: {}", err),
+            };
+        println!("Random Fact!\n{:#?}", resp);
+    }
 }
